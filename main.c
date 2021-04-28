8ea554eeca6f61fea2c72993d20e7db6c45c87c9
Sumanth181099 <65185434+Sumanth181099@users.noreply.github.com>
2021-04-28 23:04:11 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
+/*
+ */
+
 #include <avr/io.h>
 #include<util/delay.h>
 #include<avr/interrupt.h>
 #include"port.h"
+#include"usart.h"
 
 unsigned volatile FLAG = 0;
+unsigned volatile overflow;
 
 void InitADC()
 {
@@ -29,17 +34,34 @@ int main(void)
 {
     port();
     InitADC();
+    USARTInit(103);
     uint16_t temp;
+    DDRB |=(1<<PB1);
+
+    //PWM
+    TCCR1A |= (1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
+    TCCR1B |= (1<<WGM12)|(1<<CS11)|(1<<CS10);
+    DDRB |= (1<<PB1);
+
+    //Timer
+    DDRD |= (1<<PD6);
+    PORTD |= (1<<PD6);
+    TCCR0B |= (1<<CS02);
+    TIMSK0 |= (1<<TOIE0);
+    TCNT0 = 0;
+    overflow =0;
+    sei();
 
     while(1)
     {
-        // Can turn on/off any device connected to port B0 based on interrupt generated
-        if(FLAG == 1) //Switch pressed (default set to 1)
+
+        if(FLAG == 1) //Switch pressed
         {
             PORTB |=(1<<PB0);
             _delay_ms(2000);
             FLAG = 0;
             temp = ReadADC(0); //Read the temperature from sensor
+            USARTWriteChar(temp);
             _delay_ms(200);
         }
         else
@@ -47,6 +69,30 @@ int main(void)
             PORTB &=~(1<<PB0);
             _delay_ms(2000);
         }
+
+        if(overflow>=12)
+        {
+            if(TCNT0>=52)
+            {
+                PORTD ^= (1<<PD6);
+                TCNT0 = 0;
+                overflow = 0;
+            }
+
+        }
+
+        OCR1A = 100;
+        _delay_ms(200);
+        OCR1A = 250;
+        _delay_ms(200);
+        OCR1A = 400;
+        _delay_ms(200);
+        OCR1A = 650;
+        _delay_ms(200);
+        OCR1A = 900;
+        _delay_ms(200);
+        OCR1A = 1024;
+        _delay_ms(200);
 
     }
 
@@ -57,4 +103,9 @@ int main(void)
 ISR(INT0_vect)
 {
     FLAG = 1;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    overflow++;
 }
